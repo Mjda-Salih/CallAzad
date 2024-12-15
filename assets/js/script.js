31d85909e27cdbad9965e0724fe566d74268bd78
iiEnchant <nawalmaeruf2@gmail.com>
2024-12-15 20:16:09 +0000

    client reviews

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Welcome to Call Azad - Your trusted handyman services!");
 });
+document.addEventListener("scroll", () => {
+  const fadeElements = document.querySelectorAll(".fade-in");
+  fadeElements.forEach((el) => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top <= window.innerHeight) {
+      el.classList.add("show");
+    }
+  });
+});
